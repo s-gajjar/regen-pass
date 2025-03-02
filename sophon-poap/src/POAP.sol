41ee549edfdf5170eb34495a789e25a16fdcf955
Vib-UX <vibhav@instaraise.io>
2025-03-02 12:35:37 -0700

    adds poap minter

---
+++ sophon-poap/src/POAP.sol
@@ -0,0 +1,20 @@
+// filepath: /my-solidity-project/my-solidity-project/contracts/MyNFT.sol
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+contract POAP is ERC721URIStorage {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIdCounter;
+
+    constructor() ERC721("POAP", "POAP") {}
+
+    function mint(string memory uri) public {
+        uint256 tokenId = _tokenIdCounter.current();
+        _tokenIdCounter.increment();
+        _mint(msg.sender, tokenId);
+        _setTokenURI(tokenId, uri);
+    }
+}
